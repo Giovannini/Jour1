7d4ef54ad91cfe416ebb7b2ec9aeb9d3cc2155fe
roncieresi <roncieresi@eisti.eu>
2015-02-05 17:12:35 +0100

    Layer class refractored and documented

--- app/controllers/mapGene.scala
+++ app/controllers/mapGene.scala
@@ -17,15 +17,31 @@ object mapGene extends Controller{
 
   private def  main( ): String = {
     /* déterminations des bornes */
-    val octave = 60
-    val frequence = 45
+    val octave = 35
+    val frequence = 20
     val persistance= 0.5f
-    val taille_sortie = 400
-    val lissage = 4
+    val taille_sortie = 150
+    val lissage = 3
 
     val calque = Layer.filledWith0(taille_sortie,1)
     val matrice = Array.ofDim[Int](taille_sortie, taille_sortie)
 
     Layer.generateLayer(frequence,octave,persistance,lissage,calque)
+
+    //calque.toString()
+
+/*    val r = scala.util.Random
+    for (i <- 0 until taille_sortie; j <- 0 until taille_sortie)
+      matrice(i)(j) =r.nextInt(256)
+
+
+    var res=""
+    for (x <- 0 until taille_sortie){
+      for (y <- 0 until taille_sortie){
+        res=res + matrice(x)(y).toString +" | "
+      }
+      res=res + "\n"
+    }
+    res*/
   }
 }
--- app/models/map/Layer.scala
+++ app/models/map/Layer.scala
@@ -8,36 +8,46 @@ case class Layer(size: Int, persist: Float) {
   private val matrix = Array.ofDim[Int](size, size)
 
   /**
-   * Display the layer matrix
-   * @author Thomas GIOVANNINI
-   * @return a string ready to be displayed
+   * Display a matrix
+   * @author Thomas Giovannini, Simon Ronciere
+   * @return The matrix in a string
    */
   private def printMatrix(): String = {
-    matrix.map(printLine)
+    matrix.toList
+      .map(_.toList
+      .map(prettify)
+      .mkString(""))
       .mkString("\n")
   }
 
   /**
-   * Display a line of an array
-   * @author Thomas GIOVANNINI
-   * @param arrayLine the line to display
-   * @return a string ready to display
+   * Format matrix square
+   * @author Simon Ronciere
+   * @param number value of the tile
+   * @return The square in html
    */
-  def printLine(arrayLine: Array[Int]): String = {
-    arrayLine.map(prettify).mkString("")
-  }
-
   private def prettify(number: Int): String = {
     "<span style=\"width=5px;height=5px;"+{if(number<100) "background-color:blue" else "background-color:rgb("+(255-number)+","+(255-number)+","+(255-number)+")"}+"\">"+"" + "0" +"</span>"
   }
 
-  override def toString: String = printMatrix
+  /**
+   * Format matrix square
+   * @author Thomas Giovannini
+   * @return The square in html
+   */
+  override def toString: String = {
+    //"Calque: size -> " + taille + "; persist = " + persist + "\n" +
+    printMatrix
+  }
 }
 
 object Layer {
+  //last boy scoot Thomas GIOVANNINI
   /**
    * Fill the matrix with 0
    * @author Simon Ronciere
+   * @param size size of the matrix to fill
+   * @param persistence value necessary to create a layer
    * @return the filled matrix
    */
   def filledWith0(size: Int, persistence: Float): Layer = {
@@ -47,9 +57,12 @@ object Layer {
     layer
   }
 
+  //last boy scoot Thomas GIOVANNINI
   /**
    * Fill the matrix of Int with random number
    * @author Simon Ronciere
+   * @param size size of the matrix to fill
+   * @param persistence value necessary to create a layer
    * @return the filled matrix
    */
   def filledWithRandom(size: Int, persistence: Float): Layer = {
@@ -60,116 +73,169 @@ object Layer {
     layer
   }
 
-  def generateLayer(frequency: Int, octaves: Int, persistence: Float, liss: Int, layer: Layer): String = {
-    val size = layer.size
+  /**
+   * Build the matrix of a working layer
+   * @author Thomas Giovannini
+   * @param size size of the matrix to smooth
+   * @param layer to smooth
+   * @param strength intensity of the layer
+   * @param frequency frequency of the regular point
+   * @param randomLayer Layer interpolated
+   * @return the matrix of a working layer
+   */
 
-    // Generate a random layer
-    val randomLayer = Layer.filledWithRandom(size, 1)
-
-    def smooth(size: Int, layer: Layer, strength: Int): Layer = {
-      for (coordX <- 0 until size; coordY <- 0 until size) {
-        layer.matrix(coordX)(coordY) =
-          valeur_interpolee(coordX, coordY, math.pow(frequency, strength).toInt, randomLayer)
-      }
-      layer
+  def fillWorkingLayers(size: Int, layer: Layer, strength: Int, frequency: Int, randomLayer: Layer): Layer = {
+    for (coordX <- 0 until size; coordY <- 0 until size) {
+      layer.matrix(coordX)(coordY) =
+        interpolateLayer(coordX, coordY, math.pow(frequency, strength).toInt, randomLayer)
     }
+    layer
+  }
 
+  /**
+   * Sum works layers according to the persitence of each layer
+   * @author Simon Roncière
+   * @param size Size of layers
+   * @param octaves Number of layers
+   * @param persistencesSum sum of persistences
+   * @param layer layer completed by the function
+   * @param workinglayers list of working layer
+   * @return a layer build with working layers
+   */
+  def sumLayers(size: Int, octaves : Int, persistencesSum:Float, layer:Layer, workinglayers:List[Layer]):Layer={
+    for (i <- 0 until size;j <- 0 until size;n <- 0 until octaves) {
+      // somme en fonction du paramètre persist qui sert à pondérer les calques
+      layer.matrix(i)(j) += (workinglayers(n).matrix(i)(j) * workinglayers(n).persist).toInt
+      //normalisation
+      layer.matrix(i)(j) = (layer.matrix(i)(j).toFloat / persistencesSum).toInt
+    }
+    layer
+  }
+
+  /**
+   * Smooth the final matrix
+   * @author Simon Roncière
+   * @param size size of the matrix
+   * @param smoothParam intensity of the smoothing
+   * @param layerSum Layer built with the sum of working layers
+   * @return final matrix
+   */
+  def smooth (size: Int, smoothParam:Int,layerSum:Layer):Layer={
+    val smoothedMatrix = Layer.filledWith0(size, 0)
+    for (coordonne_x <- 0 until size; coordonne_y <- 0 until size) {
+      var SumTiles,NbLayers = 0
+      val sizeList = (0 until size).toList
+      //for (k <- (math.max(coordonne_x - smoothParam,0)) to (math.min(coordonne_x + smoothParam,size)); l <- (math.max(coordonne_y - smoothParam,0)) to math.min(coordonne_y + smoothParam,0)) {
+      for (k <- (coordonne_x - smoothParam) to (coordonne_x + smoothParam); l <- (coordonne_y - smoothParam) to (coordonne_y + smoothParam)) {
+        if (sizeList.contains(k) && sizeList.contains(l)) {
+          NbLayers = NbLayers + 1
+          SumTiles = SumTiles + layerSum.matrix(k)(l)
+        }
+      }
+      smoothedMatrix.matrix(coordonne_x)(coordonne_y) = SumTiles / NbLayers
+    }
+    smoothedMatrix
+  }
+
+  /**
+   * Build a layer with a Perlin noise
+   * @author Simon Roncière
+   * @param frequency frequency of the regular point
+   * @param octaves Number of layers
+   * @param persistence value necessary to create a layer
+   * @param smoothParam intensity of the smoothing
+   * @param layer initial Layer 
+   * @return A generated matrix in a string
+   */
+  def generateLayer(frequency: Int, octaves: Int, persistence: Float, smoothParam: Int, layer: Layer): String = {
+    val size = layer.size
+    // Generate a random layer
+    val randomCalque = Layer.filledWithRandom(size, 1)
+    // Generate workingLayer, they have different weighting
     val calquesTravail = generateWorkingLayer(octaves, persistence, size)
       .zipWithIndex
-      .map{ indexedCalque => smooth(size, indexedCalque._1, indexedCalque._2)}
-
-    //var res = calquesTravail.mkString("\n")
-
-    val sum_persistances = calquesTravail.map(_.persist).sum
-
-    //ajout des calques successifs
-    for (i <- 0 until size) {
-      for (j <- 0 until size) {
-        for (n <- 0 until octaves) {
-          // ! claque param
-          layer.matrix(i)(j) += (calquesTravail(n).matrix(i)(j) * calquesTravail(n).persist).toInt
-          //normalisation
-          layer.matrix(i)(j) = (layer.matrix(i)(j).toFloat / sum_persistances).toInt
-        }
-      }
-
-      //Lissage
-      /*Il est possible de carrément optimiser cette quadruple boucle en utilisant des map scala.
-      Il est pour cela nécessaire de comprendre réellement ce qu'elle fait.
-      BoyScoot Thomas*/
-      val lissage = Layer.filledWith0(size, 0)
-      val sizeList = (0 until size).toList
-      for (x <- sizeList; y <- sizeList) {
-        var a = 0
-        var n = 0
-        for (k <- (x - liss) to (x + liss); l <- (y - liss) to (y + liss)) {
-          if (sizeList.contains(k) && sizeList.contains(l)) {
-            n += 1
-            a += layer.matrix(k)(l)
-          }
-        }
-        lissage.matrix(x)(y) = a / n
-      }
-    }
-    val res=layer.toString
-    res
+      .map{ indexedCalque => fillWorkingLayers(size, indexedCalque._1, indexedCalque._2,frequency,randomCalque)}
+    val sum_persistence = calquesTravail.map(_.persist).sum
+    val sum_layer = sumLayers(size,octaves,sum_persistence,layer, calquesTravail)
+    //smoothing of the final matrix
+    val smoothedMatrix = smooth(size,smoothParam,sum_layer)
+    smoothedMatrix.toString
   }
 
   /**
    * Generate an array of working layer
    * @author Thomas GIOVANNINI
    * @param octaves the size of the array
-   * @param persistence the layer persistance
+   * @param persistance the layer persistance
    * @return the array of working layer filled with 0s
    */
-  def generateWorkingLayer(octaves: Int, persistence: Float, taille: Int): List[Layer] = {
-    new Array[Layer](octaves).zipWithIndex
-      .map{
-        l => Layer.filledWith0(taille, math.pow(persistence, l._2).toFloat)
-      }.toList
-  }
-
-  def valeur_interpolee(xCoordinate: Int, yCoordinate: Int, frequence: Int, randomLayer: Layer): Int = {
-    /* déterminations des bornes */
-    //Détermination d'un pas en fonction de la fréquence
-    val pas: Float = randomLayer.size.toFloat / frequence.toFloat
-
-    val qX: Int = (xCoordinate.toFloat / pas).toInt
-    val borneInfX = (qX * pas).toInt
-    val borneSupX = math.min(((qX + 1) * pas).toInt, randomLayer.size - 1)
-
-    val qY = (yCoordinate.toFloat / pas).toInt
-    val borneInfY = (qY * pas).toInt
-    val borneSupY = math.min(((qY + 1) * pas).toInt, randomLayer.size - 1)
-
-    /* récupérations des valeurs aléatoires aux bornes */
-    val xInfyInfBorn = randomLayer.matrix(borneInfX)(borneInfY)
-    val xInfySupBorn = randomLayer.matrix(borneInfX)(borneSupY)
-    val xSupyInfBorn = randomLayer.matrix(borneSupX)(borneInfY)
-    val xSupySupBorn = randomLayer.matrix(borneSupX)(borneSupY)
-
-    val v1 = interpolate(xInfyInfBorn, xInfySupBorn, borneSupY - borneInfY, yCoordinate - borneInfY)
-    val v2 = interpolate(xSupyInfBorn, xSupySupBorn, borneSupY - borneInfY, yCoordinate - borneInfY)
-
-    interpolate(v1, v2, borneSupX - borneInfX, xCoordinate - borneInfX)
+  def generateWorkingLayer(octaves: Int, persistance: Float, taille: Int): List[Layer] = {
+    val calquesTravail = new Array[Layer](octaves)
+    for (i <- 0 until octaves) {
+      val filledWith0Layer = Layer.filledWith0(taille, math.pow(persistance, i+1).toFloat)
+      calquesTravail(i) = filledWith0Layer
+    }
+    calquesTravail.toList
   }
 
   /**
-   * Fill a matrix of tile with empty tiles
-   * @author Simon RONCIERE
-   * @return the initialized matrix
+   * Build the bounds of values so interpolate
+   * @author Simon Roncière
+   * @param ratio Coordinate / step
+   * @param step size / frequency
+   * @param size size of the matrix where function is use
+   * @return (lower bound, highter bound)
    */
-  private def interpolate(lowerBorn: Int, upperBorn: Int, intervaleSize: Int, delta: Int): Int = {
-    // interpolation non linéaire
-    if (intervaleSize == 0) lowerBorn
-    else if (intervaleSize == 1) upperBorn
-    else {
-      val a = delta.toFloat / intervaleSize
-      val fac1 = 3 * math.pow(1 - a, 2) - 2 * math.pow(1 - a, 3)
-      val fac2 = 3 * math.pow(a, 2) - 2 * math.pow(a, 3)
+  def buildBounds(ratio:Int, step: Float,size:Int):(Int,Int)={
+    var SupBound = ((ratio + 1) * step).toInt
+    if (SupBound >= size) SupBound = size- 1
+    ((ratio * step).toInt,SupBound)
+  }
 
-      (lowerBorn * fac1 + upperBorn * fac2).toInt
-    }
+  /**
+   * Interpolate a point in a layer
+   * @author Simon Roncière
+   * @param xCoordinate coordinate x of the considered point
+   * @param yCoordinate coordinate x of the considered point
+   * @param frequency frequency of the regular point
+   * @param randomLayer Layer interpolated
+   * @return value of the point interpolate
+   */
+  def interpolateLayer(xCoordinate: Int, yCoordinate: Int, frequency: Int, randomLayer: Layer): Int = {
 
+    val step: Float = randomLayer.size.toFloat / frequency.toFloat
+
+    val (infBoundX,supBoundX)=buildBounds((xCoordinate.toFloat / step).toInt,step,randomLayer.size)
+    val (infBoundY,supBoundY)=buildBounds((yCoordinate.toFloat / step).toInt,step,randomLayer.size)
+    
+    /* récupérations des valeurs aléatoires aux bornes */
+    val valInfxInfy = randomLayer.matrix(infBoundX)(infBoundY)
+    val valInfxSupy = randomLayer.matrix(infBoundX)(supBoundY)
+    val valSupxInfy = randomLayer.matrix(supBoundX)(infBoundY)
+    val valSupxSupy = randomLayer.matrix(supBoundX)(supBoundY)
+
+    val interpolateInfx = interpolate(valInfxInfy, valInfxSupy, supBoundY - infBoundY, yCoordinate - infBoundY)
+    val interpolateSupx = interpolate(valSupxInfy, valSupxSupy, supBoundY - infBoundY, yCoordinate - infBoundY)
+    interpolate(interpolateInfx, interpolateSupx, supBoundX - infBoundX, xCoordinate - infBoundX)
+
+  }
+
+  /**
+   * polynomiale interpolation of values
+   * @param val1 first value
+   * @param val2 second value
+   * @param intervalWidth width of the interval beetween the values
+   * @param delta distance to the lower bound
+   * @return value interpolate
+   */
+  private def interpolate(val1: Int, val2: Int, intervalWidth: Int, delta: Int): Int = {
+    if (intervalWidth == 0)
+      return val1
+    if (intervalWidth == 1)
+      return val2
+    val a = delta / intervalWidth
+    val fac1 = 3 * math.pow(1 - a, 2) - 2 * math.pow(1 - a, 3)
+    val fac2 = 3 * math.pow(a, 2) - 2 * math.pow(a, 3)
+    (val1 * fac1 + val2 * fac2).toInt
   }
 }
