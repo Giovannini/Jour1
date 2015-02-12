5b148aaa66d195287ec7e420fe2f8ae6ae406e74
Thomas Giovannini <thomas.giovannini@eisti.fr>
2015-02-12 10:01:35 +0100

    Small modifications on WorldInit

--- app/models/map/WorldInit.scala
+++ app/models/map/WorldInit.scala
@@ -14,7 +14,7 @@ object WorldInit {
    * @param groundIdsList the list of the ground strength
    * @return the sum of the strength of all the ground concepts
    */
-  def getStrengthSum(groundIdsList:List[Int]): Int = {
+  def getStrengthSum(groundIdsList: List[Int]): Int = {
     groundIdsList.map(Concept.getById)
       .map {
       case Some(concept) => getStrengthOf(concept)
@@ -45,38 +45,34 @@ object WorldInit {
    * @param listGrounds the ground list
    * @return a list of tuple containing whatever is "maxOccupe" and its associated concept
    */
-
-  def repartition(lastBounds: Int, lowestMatrixValue : Int, biggestMatrixValue :Int, listGrounds:List[Int] , sumStr:Int): List[(Int,Concept)] ={
+  def repartition(lastBound: Int, lowestMatrixValue: Int, biggestMatrixValue: Int, listGrounds: List[Int], sumStr: Int): List[(Int, Concept)] = {
     listGrounds match {
-      case head::Nil => Concept.getById(head) match {
+      case head :: tail => Concept.getById(head) match {
         case Some(concept) =>
-          (biggestMatrixValue, concept)::Nil
-        case _ => Nil
-      }
-
-      case head::tail => Concept.getById(head) match {
-        case Some(concept) =>
-          val maxOccupe = getMaxOccupe(lastBounds,lowestMatrixValue, biggestMatrixValue, sumStr, concept)
+          val maxOccupe = getMaxOccupe(lastBound, lowestMatrixValue, biggestMatrixValue, sumStr, concept, tail.length)
           (maxOccupe, concept) :: repartition(maxOccupe, lowestMatrixValue, biggestMatrixValue, tail, sumStr)
-        case _ => repartition(lastBounds, lowestMatrixValue, biggestMatrixValue, tail, sumStr)
+        case _ => repartition(lastBound, lowestMatrixValue, biggestMatrixValue, tail, sumStr)
       }
       case _ => List()
     }
 
   }
 
-  def shortIncreasing (list:List[(Int,Concept)]):List[(Int,Concept)]={
+  def shortIncreasing(list: List[(Int, Concept)]): List[(Int, Concept)] = {
     list.sortBy(_._1)
   }
+
   /**
    * Get what Simon calls the maxOccupe
    * @author Thomas GIOVANNINI
-   * @param lastBounds
+   * @param lastBound
    * @param concept
    * @return whatever maxOccupe is, it returns it
    */
-  def getMaxOccupe(lastBounds: Int, lowestMatrixValue:Int, biggestMatrixValue:Int, sumStr:Int, concept: Concept): Int = {
-    lastBounds + (getStrengthOf(concept) * (biggestMatrixValue - lowestMatrixValue) / sumStr)
+  def getMaxOccupe(lastBound: Int, lowestMatrixValue: Int, biggestMatrixValue: Int, sumStr: Int, concept: Concept, remainingElements: Int): Int = {
+    if (remainingElements > 0)
+      lastBound + (getStrengthOf(concept) * (biggestMatrixValue - lowestMatrixValue) / sumStr)
+    else biggestMatrixValue
   }
 
   /**
@@ -96,28 +92,29 @@ object WorldInit {
    * @param heigth
    * @return
    */
-  def worldMapGeneration(width:Int,heigth:Int): List[Instance] ={
+  def worldMapGeneration(width: Int, heigth: Int): List[Instance] = {
     val frequency = 20
     val octave = 35
-    val persistence= 0.5f
+    val persistence = 0.5f
     val smoothed = 3
     val outputSize = width
 
-    val layer = Layer.generateLayer(frequency,octave,persistence,smoothed,outputSize)
+    val layer = Layer.generateLayer(frequency, octave, persistence, smoothed, outputSize)
 
-    val idGround=getGround(Concept.findAll)
-    val listGrounds=getGrounds(idGround::Nil)
-    val sumStr= sumStrength(listGrounds)
-    println("SumStr grounds = "+sumStr)
-    val (min,moy,max)=layer.statMatrix
-    val repartitionList = repartition(min, min,max,listGrounds,sumStr).sortBy(_._1)
+    val idGround = getGround(Concept.findAll)
+    val listGrounds = getGrounds(idGround :: Nil)
+    val sumStr = sumStrength(listGrounds)
+    println("SumStr grounds = " + sumStr)
+    val (min, moy, max) = layer.statMatrix
+    val repartitionList = repartition(min, min, max, listGrounds, sumStr).sortBy(_._1)
     //
 
     val terrain = matrixToList(layer.matrix)
-      .map{createInstance(_,repartitionList)
+      .map {
+      createInstance(_, repartitionList)
     }
 
-    terrain:::randomObjectInstanciation(listGrounds,outputSize,outputSize)
+    terrain ::: randomObjectInstanciation(listGrounds, outputSize, outputSize)
   }
 
   /**
@@ -127,11 +124,11 @@ object WorldInit {
    * @return
    */
 
-  def sumStrength(listConcept : List[Int]):Int={
-    listConcept match{
+  def sumStrength(listConcept: List[Int]): Int = {
+    listConcept match {
       case Nil => 0
-      case head::tail => Concept.getById(head) match {
-        case Some(c) => getStrengthOf(c)+sumStrength(tail)
+      case head :: tail => Concept.getById(head) match {
+        case Some(c) => getStrengthOf(c) + sumStrength(tail)
         case _ => sumStrength(tail)
 
       }
@@ -143,9 +140,9 @@ object WorldInit {
    * @param matrix
    * @return
    */
-  def matrixToList(matrix: Array[Array[Int]]):List[(Int,Int,Int)]={
+  def matrixToList(matrix: Array[Array[Int]]): List[(Int, Int, Int)] = {
     matrix.map(_.toList.zipWithIndex).toList.zipWithIndex
-      .map{
+      .map {
       indexedLine => indexedLine._1.map(element => (element._1, element._2, indexedLine._2))
     }.flatten
   }
@@ -156,8 +153,8 @@ object WorldInit {
    * @param repartitionList
    * @return
    */
-  def createInstance(triplet:(Int,Int,Int),repartitionList: List[(Int,Concept)]):Instance={
-    Instance.createRandomInstanceOf(instanciationTile(triplet._1,repartitionList)).at(Coordinates(triplet._2,triplet._3))
+  def createInstance(triplet: (Int, Int, Int), repartitionList: List[(Int, Concept)]): Instance = {
+    Instance.createRandomInstanceOf(instanciationTile(triplet._1, repartitionList)).at(Coordinates(triplet._2, triplet._3))
   }
 
   /**
@@ -166,13 +163,13 @@ object WorldInit {
    * @param list
    * @return
    */
-  def instanciationTile(valeur:Int,list:List[(Int,Concept)]):Concept={
-    list match{
+  def instanciationTile(valeur: Int, list: List[(Int, Concept)]): Concept = {
+    list match {
       case Nil => Concept.error
-      case _ => if(valeur<list.head._1)
+      case _ => if (valeur < list.head._1)
         list.head._2
       else
-        instanciationTile(valeur,list.tail)
+        instanciationTile(valeur, list.tail)
     }
 
 
@@ -183,9 +180,9 @@ object WorldInit {
    * @param concepts
    * @return -1 if concept "Ground" not found, else id of the concept
    */
-  def getGround(concepts:List[Concept]): Int ={
-    concepts.find(_.label=="Ground") match {
-      case Some(c)=> c.id
+  def getGround(concepts: List[Concept]): Int = {
+    concepts.find(_.label == "Ground") match {
+      case Some(c) => c.id
       case _ => -1
     }
   }
@@ -195,12 +192,12 @@ object WorldInit {
    * @param listID
    * @return
    */
-  def getGrounds(listID:List[Int]):List[Int]={
+  def getGrounds(listID: List[Int]): List[Int] = {
     listID match {
       case Nil => Nil
-      case head::tail => val listChild = Concept.getChildren(head)
-        if (listChild.isEmpty)head::getGrounds(tail)
-        else getGrounds(tail:::listChild.map(_.id))
+      case head :: tail => val listChild = Concept.getChildren(head)
+        if (listChild.isEmpty) head :: getGrounds(tail)
+        else getGrounds(tail ::: listChild.map(_.id))
     }
 
 
@@ -210,22 +207,19 @@ object WorldInit {
    * @author Thomas Giovannini
    * @param listConcepts
    */
-  def compTile(listConcepts: List[Concept]):Unit={
+  def compTile(listConcepts: List[Concept]): Unit = {
 
   }
-  def instConcept(coordinates:Coordinates,concept:Concept):Instance={
-    val prop=List(
+
+  def instConcept(coordinates: Coordinates, concept: Concept): Instance = {
+    val prop = List(
       ValuedProperty(Property("Nom", "String", "GIOVA"), concept.label),
       ValuedProperty(Property("Id", "Int", 0), (math.random * 10).toInt.toString))
-    Instance(0, concept.label,coordinates,prop,concept)
+    Instance(0, concept.label, coordinates, prop, concept)
   }
-
-
 
 
   ///////////////////////////::OBJECT:://///////////////////////////////
-
-
 
 
   /**
@@ -234,9 +228,9 @@ object WorldInit {
    * @param listConcepts list concept where we get instanciables
    * @return
    */
-  def getInstanciable(listConcepts:List[Concept]): List[Concept] ={
-    val propertyInstanciable      = Property("Instanciable", "Boolean", false)
-    listConcepts.filter(concept=>concept.rules.contains(ValuedProperty(propertyInstanciable,true)))
+  def getInstanciable(listConcepts: List[Concept]): List[Concept] = {
+    val propertyInstanciable = Property("Instanciable", "Boolean", false)
+    listConcepts.filter(concept => concept.rules.contains(ValuedProperty(propertyInstanciable, true)))
   }
 
   /**
@@ -246,24 +240,24 @@ object WorldInit {
    * @param concepts
    * @return
    */
-  def getInstance(grounds:List[Int], concepts:List[Concept]):List[Int]={
-     val allInst:List[Concept] = getInstanciable(concepts)
-      allInst match {
+  def getInstance(grounds: List[Int], concepts: List[Concept]): List[Int] = {
+    val allInst: List[Concept] = getInstanciable(concepts)
+    allInst match {
       case Nil => Nil
-      case head::Nil => 
-        if(!grounds.contains(head.id))head.id::Nil
+      case head :: Nil =>
+        if (!grounds.contains(head.id)) head.id :: Nil
         else Nil
-      case head::tail => 
-        if(grounds.contains(head.id))getInstance(grounds,tail) 
-        else head.id::getInstance(grounds,tail)
+      case head :: tail =>
+        if (grounds.contains(head.id)) getInstance(grounds, tail)
+        else head.id :: getInstance(grounds, tail)
     }
   }
 
-  def getNbTileByInstance(listInstanciable:List[Int],sumStrO:Int,nbTile:Int,vide:Int): List[(Int,Concept)] ={
-    if(sumStrO==0 || vide==0){
+  def getNbTileByInstance(listInstanciable: List[Int], sumStrO: Int, nbTile: Int, vide: Int): List[(Int, Concept)] = {
+    if (sumStrO == 0 || vide == 0) {
       println("erreur")
       Nil
-    }else {
+    } else {
       listInstanciable match {
         case Nil => Nil
         case head :: tail =>
@@ -277,23 +271,21 @@ object WorldInit {
     }
   }
 
-  def randomObjectInstanciation(grounds:List[Int],heigth:Int,width:Int): List[Instance] ={
-    val nbTile = heigth*width
-    val listInstance= getInstance(grounds,Concept.findAll)
+  def randomObjectInstanciation(grounds: List[Int], heigth: Int, width: Int): List[Instance] = {
+    val nbTile = heigth * width
+    val listInstance = getInstance(grounds, Concept.findAll)
     val sumStr = sumStrength(listInstance)
-    println("NbTile = "+nbTile)
-    println("SumStr objets = "+sumStr)
-    val listNumberEachInstance = getNbTileByInstance(listInstance,sumStr,nbTile,2)
-    listNumberEachInstance.map(tuple=>println("concept "+tuple._2.label+" = "+tuple._1))
-    listNumberEachInstance.map(tuple=>putInstanciesOfOneConcept(tuple._1,tuple._2,heigth,width)).flatten
+    println("NbTile = " + nbTile)
+    println("SumStr objets = " + sumStr)
+    val listNumberEachInstance = getNbTileByInstance(listInstance, sumStr, nbTile, 2)
+    listNumberEachInstance.map(tuple => println("concept " + tuple._2.label + " = " + tuple._1))
+    listNumberEachInstance.map(tuple => putInstanciesOfOneConcept(tuple._1, tuple._2, heigth, width)).flatten
   }
 
 
-
-
-  def putInstanciesOfOneConcept(nbTile:Int,concept:Concept,heigth:Int,width:Int): List[Instance] ={
+  def putInstanciesOfOneConcept(nbTile: Int, concept: Concept, heigth: Int, width: Int): List[Instance] = {
     val r = scala.util.Random
-    Seq.fill(nbTile)(Instance.createRandomInstanceOf(concept).at(Coordinates(r.nextInt(heigth),r.nextInt(width)))).toList
+    Seq.fill(nbTile)(Instance.createRandomInstanceOf(concept).at(Coordinates(r.nextInt(heigth), r.nextInt(width)))).toList
   }
 
 
