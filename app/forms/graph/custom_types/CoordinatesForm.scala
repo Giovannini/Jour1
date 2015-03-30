be78d9cca6ac3214232e23708dc5b63ce01eeeef
Aurélie Lorgeoux <aurelie.lorgeoux@eisti.fr>
2015-03-30 19:11:09 +0200

    Edition d'une instance

---
+++ app/forms/graph/custom_types/CoordinatesForm.scala
@@ -0,0 +1,8 @@
+package forms.graph.custom_types
+
+/**
+ * Created by lorgeouxau on 3/30/15.
+ */
+class CoordinatesForm {
+
+}
---
+++ app/forms/graph/ontology/InstanceForm.scala
@@ -0,0 +1,8 @@
+package forms.graph.ontology
+
+/**
+ * Created by lorgeouxau on 3/30/15.
+ */
+object InstanceForm {
+
+}
